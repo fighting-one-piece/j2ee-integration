3b34f99f4dbf83f208a03507d2781a98c9131753
wulin <125906088@qq.com>
2015-02-16 11:14:36 +0800

    scheduler module code commit

--- integration-scheduler/src/main/java/org/platform/modules/scheduler/factory/SchedulerFactoryExtBean.java
+++ integration-scheduler/src/main/java/org/platform/modules/scheduler/factory/SchedulerFactoryExtBean.java
@@ -4,7 +4,6 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 public class SchedulerFactoryExtBean extends SchedulerFactoryBean {
 
-	
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		if (isAutoStartup()) {
